7b9b6a82f2b14ea7bd51fa2a92ba34ecb912094a
FranklinOtero10 <franklin.otero6@gmail.com>
2021-05-20 15:08:00 -0600

    Terminado

---
+++ ADSProject/Models/Grupo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ADSProject.Models
+{
+    [Table("Grupo")]
+    public class Grupo
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        public String Nombre { get; set; }
+
+        [Display(Name = "ID Carrera")]
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        public int idCarrera { get; set; }
+
+        [Display(Name = "ID Materia")]
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        public int idMateria { get; set; }
+
+        [Display(Name = "ID Profesor")]
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        public int idProfesor { get; set; }
+
+        [Display(Name = "Ciclo")]
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        public int Ciclo { get; set; }
+
+        [Display(Name = "Año")]
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        public int Anio { get; set; }
+    }
+}
---
+++ ADSProject/Services/ServiceEstudiantes.cs
@@ -0,0 +1,110 @@
+﻿using ADSProject.DAL;
+using ADSProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ADSProject.Models.Context;
+
+namespace ADSProject.Services
+{
+    public class ServiceEstudiantes
+    {
+        // Instancia para acceder a todos los metodos de la DAL
+       // public EstudianteDAL estudianteDal = new EstudianteDAL();
+
+        // Para insertar estudiante
+        public int insertar(Estudiante estudiante)
+        {
+            try
+            {
+                //Inicializar el proceso para conectarnos a la bd
+                using (MyDbContext context = new MyDbContext())
+                {
+                    //crear instancia de la DAL y le pasamos el context
+                    EstudianteDAL dal = new EstudianteDAL(context);
+                    //Llamada para obtener el metodo de todos los registros
+                    return dal.insertarEstudiante(estudiante);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // Para modificar un estudiante
+        public int modificar( int id, Estudiante estudiante)
+        {
+            try
+            {
+                //Inicializar el proceso para conectarnos a la bd
+                using (MyDbContext context = new MyDbContext())
+                {
+                    //crear instancia de la DAL y le pasamos el context
+                    EstudianteDAL dal = new EstudianteDAL(context);
+                    //Llamada para obtener el metodo de todos los registros
+                    return dal.modificarEstudiante(id,estudiante);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // Para eliminar
+
+        public bool eliminar( int id)
+        {
+            try
+            {
+                //Inicializar el proceso para conectarnos a la bd
+                using (MyDbContext context = new MyDbContext())
+                {
+                    //crear instancia de la DAL y le pasamos el context
+                    EstudianteDAL dal = new EstudianteDAL(context);
+                    //Llamada para obtener el metodo de todos los registros
+                    return dal.eliminarEstudiante(id);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        // Para obtener todos los estudiantes.
+        public List<Estudiante> obtenerTodos()
+        {
+            //Inicializar el proceso para conectarnos a la bd
+            using (MyDbContext context = new MyDbContext())
+            {
+                //crear instancia de la DAL y le pasamos el context
+                EstudianteDAL dal = new EstudianteDAL(context);
+                //Llamada para obtener el metodo de todos los registros
+                return dal.obtenerTodos();
+            }
+        }
+
+        // Para obtener por ID.
+        public Estudiante obtenerPorID( int id)
+        {
+            try
+            {
+                //Inicializar el proceso para conectarnos a la bd
+                using (MyDbContext context = new MyDbContext())
+                {
+                    //crear instancia de la DAL y le pasamos el context
+                    EstudianteDAL dal = new EstudianteDAL(context);
+                    //Llamada para obtener el metodo de todos los registros
+                    return dal.obtenerPorID(id);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
